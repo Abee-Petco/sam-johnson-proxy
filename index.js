c094c4aa575dc5e62b79c3ab0a60e0dee5e30160
Sam Johnson <samj@iconcollective.com>
2020-10-07 17:41:37 -0500

    removed log

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const redisMiddleware = (req, res, next) => {
   let key = '__expIress' + req.originalUrl || req.url;
   client.get(key, function (err, reply) {
     if (reply) {
-      console.log('from cache!');
       res.send(reply);
     } else {
       res.sendResponse = res.send;
